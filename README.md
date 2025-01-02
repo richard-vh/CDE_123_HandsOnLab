487861d0ae13a300ed0739e7584f3e38d8b376b8
pdefusco <pauldefusco@gmail.com>
2025-01-02 14:57:54 -0800

    genesis

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
-# CDE Hands On Lab
+# CDE 1.23 Hands On Lab
 
-This Lab is WIP!
+## About the CDE Lab
+
+In these Labs you will gain hands-on experience the Cloudera Data Engineering Service. In this excercise you will:
+
+1. Develop a PySpark and Iceberg Application with Spark 3.5 connecting to a Spark Virtual Cluster via Spark Connect using the VSCode IDE.
+2. Prototype your Spark Application in a dedicated CDE DEV environment.
+3. Use git to back up your code and push it to a dedicated CDE PRD environment.
+4. Create an Airflow Pipeline to orchestrate multiple Spark Applications.
+5. Monitor your Spark Applications with CDP Observability and the CDP Data Catalog.
+
+## About the Cloudera Data Engineering (CDE) Service
+
+CDE is the Cloudera Data Engineering Service, a containerized managed service for Cloudera Data Platform designed for Large Scale Batch Pipelines with Spark, Airflow and Iceberg. It allows you to submit batch jobs to auto-scaling virtual clusters. As a Cloud-Native service, CDE enables you to spend more time on your applications, and less time on infrastructure.
+
+CDE allows you to create, manage, and schedule Apache Spark jobs without the overhead of creating and maintaining Spark clusters. With CDE, you define virtual clusters with a range of CPU and memory resources, and the cluster scales up and down as needed to run your Spark workloads, helping to control your cloud costs.
+
+## Step by Step Instructions
+
+Detailed instructions are provided in the [step_by_step_guides](https://github.com/pdefusco/CDE_121_HOL/tree/main/step_by_step_guides) folder.
+
+* [Link to the English Guide]().
+* [Enlace a la Guía en Español]()
+* [Lien vers le Guide en Français]()
+* [Link per la Guida in Italiano]()
+* [Link para o Guia em Português]()
---
+++ pyspark-app.py
@@ -0,0 +1,101 @@
+#****************************************************************************
+# (C) Cloudera, Inc. 2020-2023
+#  All rights reserved.
+#
+#  Applicable Open Source License: GNU Affero General Public License v3.0
+#
+#  NOTE: Cloudera open source products are modular software products
+#  made up of hundreds of individual components, each of which was
+#  individually copyrighted.  Each Cloudera open source product is a
+#  collective work under U.S. Copyright Law. Your license to use the
+#  collective work is as provided in your written agreement with
+#  Cloudera.  Used apart from the collective work, this file is
+#  licensed for your use pursuant to the open source license
+#  identified above.
+#
+#  This code is provided to you pursuant a written agreement with
+#  (i) Cloudera, Inc. or (ii) a third-party authorized to distribute
+#  this code. If you do not have a written agreement with Cloudera nor
+#  with an authorized and properly licensed third party, you do not
+#  have any rights to access nor to use this code.
+#
+#  Absent a written agreement with Cloudera, Inc. (“Cloudera”) to the
+#  contrary, A) CLOUDERA PROVIDES THIS CODE TO YOU WITHOUT WARRANTIES OF ANY
+#  KIND; (B) CLOUDERA DISCLAIMS ANY AND ALL EXPRESS AND IMPLIED
+#  WARRANTIES WITH RESPECT TO THIS CODE, INCLUDING BUT NOT LIMITED TO
+#  IMPLIED WARRANTIES OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY AND
+#  FITNESS FOR A PARTICULAR PURPOSE; (C) CLOUDERA IS NOT LIABLE TO YOU,
+#  AND WILL NOT DEFEND, INDEMNIFY, NOR HOLD YOU HARMLESS FOR ANY CLAIMS
+#  ARISING FROM OR RELATED TO THE CODE; AND (D)WITH RESPECT TO YOUR EXERCISE
+#  OF ANY RIGHTS GRANTED TO YOU FOR THE CODE, CLOUDERA IS NOT LIABLE FOR ANY
+#  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR
+#  CONSEQUENTIAL DAMAGES INCLUDING, BUT NOT LIMITED TO, DAMAGES
+#  RELATED TO LOST REVENUE, LOST PROFITS, LOSS OF INCOME, LOSS OF
+#  BUSINESS ADVANTAGE OR UNAVAILABILITY, OR LOSS OR CORRUPTION OF
+#  DATA.
+#
+# #  Author(s): Paul de Fusco
+#***************************************************************************/
+
+from os.path import exists
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+from pyspark.sql.types import *
+
+spark = SparkSession \
+    .builder \
+    .appName("BANK TRANSACTIONS BRONZE LAYER") \
+    .getOrCreate()
+
+storageLocation = "s3a://go01-demo/data"
+username = "user001"
+
+### LOAD HISTORICAL TRANSACTIONS FILE FROM CLOUD STORAGE
+transactionsDf = spark.read.json("{0}/mkthol/trans/{1}/rawtransactions".format(storageLocation, username))
+transactionsDf.printSchema()
+
+spark.sql("CREATE DATABASE IF NOT EXISTS SPARK_CATALOG.HOL_DB_{}".format(username))
+spark.sql("DROP TABLE IF EXISTS spark_catalog.HOL_DB_{0}.TRANSACTIONS_{0} PURGE".format(username))
+
+transactionsDf.writeTo("SPARK_CATALOG.HOL_DB_{0}.TRANSACTIONS_{0}".format(username)).using("iceberg").tableProperty("write.format.default", "parquet").createOrReplace()
+
+trxBatchDf = spark.read.schema("credit_card_number string, credit_card_provider string, event_ts timestamp, latitude double, longitude double, transaction_amount long, transaction_currency string, transaction_type string").json("{0}/mkthol/trans/{1}/trx_batch_1".format(storageLocation, username))
+trxBatchDf.createOrReplaceTempView("trx_batch")
+
+# PRE-MERGE COUNTS BY TRANSACTION TYPE:
+spark.sql("""SELECT TRANSACTION_TYPE, COUNT(*) FROM spark_catalog.HOL_DB_{0}.TRANSACTIONS_{0} GROUP BY TRANSACTION_TYPE""".format(username)).show()
+
+# MERGE OPERATION
+spark.sql("""MERGE INTO spark_catalog.HOL_DB_{0}.TRANSACTIONS_{0} t   
+USING (SELECT * FROM trx_batch) s          
+ON t.credit_card_number = s.credit_card_number               
+WHEN MATCHED AND t.transaction_amount < 1000 AND t.transaction_currency != "CHF" THEN UPDATE SET t.transaction_type = "invalid"
+WHEN NOT MATCHED THEN INSERT *""".format(username))
+
+# POST-MERGE COUNT:
+spark.sql("""SELECT TRANSACTION_TYPE, COUNT(*) FROM spark_catalog.HOL_DB_{0}.TRANSACTIONS_{0} GROUP BY TRANSACTION_TYPE""".format(username)).show()
+
+# ICEBERG TABLE HISTORY (SHOWS EACH SNAPSHOT AND TIMESTAMP)
+spark.sql("SELECT * FROM spark_catalog.HOL_DB_{0}.TRANSACTIONS_{0}.history".format(username)).show()
+
+# ICEBERG TABLE SNAPSHOTS (USEFUL FOR INCREMENTAL QUERIES AND TIME TRAVEL)
+spark.sql("SELECT * FROM spark_catalog.HOL_DB_{0}.TRANSACTIONS_{0}.snapshots".format(username)).show()
+
+# APPEND SECOND DATA BATCH
+trxBatchDf = spark.read.schema("credit_card_number string, credit_card_provider string, event_ts timestamp, latitude double, longitude double, transaction_amount long, transaction_currency string, transaction_type string").json("{0}/mkthol/trans/{1}/trx_batch_2".format(storageLocation, username))
+trxBatchDf.writeTo("spark_catalog.HOL_DB_{0}.TRANSACTIONS_{0}".format(username)).using("iceberg").append()
+
+# STORE FIRST AND LAST SNAPSHOT ID'S FROM SNAPSHOTS TABLE
+snapshots_df = spark.sql("SELECT * FROM spark_catalog.HOL_DB_{0}.TRANSACTIONS_{0}.snapshots;".format(username))
+
+last_snapshot = snapshots_df.select("snapshot_id").tail(1)[0][0]
+second_snapshot = snapshots_df.select("snapshot_id").collect()[1][0]
+
+incReadDf = spark.read\
+    .format("iceberg")\
+    .option("start-snapshot-id", second_snapshot)\
+    .option("end-snapshot-id", last_snapshot)\
+    .load("spark_catalog.HOL_DB_{0}.TRANSACTIONS_{0}".format(username))
+
+print("Incremental Report:")
+incReadDf.show()
